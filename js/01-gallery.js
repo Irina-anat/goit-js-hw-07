64fd0c6df36823279a0f263e9c6e9ca7fa7e7e2a
Iryna Rozmaita <iriszechka@ukr.net>
2023-03-31 18:53:35 +0300

    02-ligthbox

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -4,6 +4,7 @@ const gallery = document.querySelector(`.gallery`);
 console.log(gallery);
 
 function markupGallery(galleryItems) {
+   // перебираю картинки, на кожній ітерації отрим об і деструкуризую
     const markup = galleryItems.map(({ preview, original, description }) => 
 `<li class="gallery__item">
   <a class="gallery__link" href="${original}">
@@ -16,27 +17,32 @@ function markupGallery(galleryItems) {
   </a>
 </li>`
     )
-    .join("");
+    .join(""); //кожен ел масиву прриводжу до рядка
   return markup; 
 };
 
-gallery.innerHTML = markupGallery(galleryItems);
+gallery.innerHTML = markupGallery(galleryItems);  //додаю розмітку
 
 gallery.addEventListener('click', onClickFoto)
+/*galleryList.insertAdjacentHTML("beforeend", markupGallery(galleryItems));*/
 
 function onClickFoto(event) {
-    event.preventDefault();   
 
+    event.preventDefault();   //скасовую дію браузера за замовч
+    // якщо ел не містить gallery__image - дія не виконується
     if(!event.target.classList.contains(`gallery__image`)){
         return;
     }
-  //console.log(event.target)
+  //console.log(event.target)  // опрацьовую подію
   const currentCard = event.target.closest(`.gallery__image`);
- //console.log(currentCard)
+  //повертає першого батька з класом gallery__image
+  console.log(currentCard)
   const originalCard = event.target.dataset.source; 
-  
+  const descriptionCard = event.target.alt;
+  console.log(descriptionCard)
+
   const instance = basicLightbox.create(`
-  <img style="border-radius: 4%"  src="${originalCard}">`,
+  <img style="border-radius: 4%"  src="${originalCard}" alt="${descriptionCard}">`,
     
     {
       onShow: () => {
@@ -61,7 +67,7 @@ function onClickFoto(event) {
 
 console.log(galleryItems);
 
- /*galleryList.insertAdjacentHTML("beforeend", markupGallery(galleryItems));*/
+ 
 
 
 
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,34 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+
+
+const gallery = document.querySelector(`.gallery`);
+console.log(gallery);
+
+function markupGallery(galleryItems) {
+   
+    const markup = galleryItems.map(({ preview, original, description }) => 
+`<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+</li>`
+    )
+    .join(""); 
+  return markup; 
+};
+
+gallery.innerHTML = markupGallery(galleryItems); 
+
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+});
+
+
 
 console.log(galleryItems);
